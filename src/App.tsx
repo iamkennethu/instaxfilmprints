c9230954cbcedd8797a1e09f6a586090bfdc1eff
iamkennethu <iam.kennethu@gmail.com>
2022-12-30 02:46:38 +0800

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import { Home } from './components/Home';
+import About from './components/About';
+import SpecialOffers from './components/SpecialOffers';
+import Location from './components/Location';
+import Guidelines from './components/Guidelines';
+import FAQs from './components/FAQs';
+
 
 function App() {
   return (
     <div className='App'>
     <BrowserRouter>
     <Routes>
-      <Route path='/home' element={<Home/>}/>
+      <Route path='/Home' element={<Home />}/>
+      <Route path='/About' element={<About />}/>
+      <Route path='/SpecialOffers' element={<SpecialOffers />}/>
+      <Route path='/Location' element={<Location />}/>
+      <Route path='/Guidelines' element={<Guidelines />}/>
+      <Route path='/FAQs' element={<FAQs />}/>
     </Routes>
     </BrowserRouter>
     </div>
