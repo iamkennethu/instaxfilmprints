1a8e60f2ba2110fc01336c37cd95b1e59b18fd56
iamkennethu <iam.kennethu@gmail.com>
2022-12-31 05:19:45 +0800

    Commit

--- src/Navbar/NavBar.tsx
+++ src/Navbar/NavBar.tsx
@@ -1,43 +1,24 @@
-import React, {useState} from "react";
-import logo from '../images/logo.png'
-import { Link } from "react-scroll";
+import React, { useState } from "react";
+import './main.css';
 
-function NavBar() {
-
-    const [nav,setnav] = useState(false);
-
-    const changeBackground = () => {
-        if (window.scrollY >= 50) {
-            setnav(true);
-
-        }
-        else {
-            setnav(false);
-        }
-    }
-    window.addEventListener('scroll',changeBackground);
-
-    return(
-        <nav className={nav ? "nav active" :"nav"}>
-            <Link to='#' className='logo'>
-                <img src={logo} alt="" />
-            </Link>
-            <input className='menu-btn' type='checkbox' id='menu-btn'/>
-                <label className='menu icon' htmlFor='menu-btn'>
-                    <span className='nav-icon'></span>
-                </label>
-                <ul className='menu'>
-                <a href="/home"> Home </a>
-                <li><Link to='#'>About</Link></li>
-                <li><Link to='#'>Special Offer</Link></li>
-                <li><Link to='#'>Location</Link></li>
-                <li><Link to='#'>Guidelines</Link></li>
-                <li><Link to='#'>FAQs</Link></li>
-
-                </ul>
-        </nav>
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div className="Navbar">
+            <span className="nav-logo">INSTAX FILM PRINTS PH</span>
+            <div className={`nav-items ${isOpen && "Open"}`}></div>
+            <a href="/home"> Home </a>
+            <a href="/About"> About </a>
+            <a href="/SpecialOffers"> Special Offers </a>
+            <a href="/Location"> Location </a>
+            <a href="/Guidelines"> Guidelines </a>
+            <a href="/FAQs"> FAQs </a>
+            <div className={`nav-toggle ${isOpen && "Open"}`} 
+            onClick={() => setIsOpen(!isOpen)}>
+                <div className="bar"></div>
+            </div>
+        </div>
     )
-
 }
 
-export default NavBar;
+export default Navbar;
