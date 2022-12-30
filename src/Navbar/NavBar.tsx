10d19c3716882c45bf734ef00cb325ff2fca3088
iamkennethu <iam.kennethu@gmail.com>
2022-12-31 01:47:25 +0800

    Commit

--- src/Navbar/NavBar.tsx
+++ src/Navbar/NavBar.tsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import './NavBar.css';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className="Navbar">
-            <span className="nav-logo">Instax Film Print PH</span>
-            <div className="nav-items"></div>
+            <span className="nav-logo">DevLHB</span>
+            <div className={`nav-items ${isOpen && "Open"}`}></div>
             <a href="/home"> Home </a>
             <a href="/About"> About </a>
             <a href="/SpecialOffers"> Special Offers </a>
             <a href="/Location"> Location </a>
             <a href="/Guidelines"> Guidelines </a>
             <a href="/FAQs"> FAQs </a>
-            <div className="nav-toggle">
+            <div className={`nav-toggle ${isOpen && "Open"}`} onClick={() => setIsOpen(!isOpen)}>
                 <div className="bar"></div>
             </div>
         </div>
