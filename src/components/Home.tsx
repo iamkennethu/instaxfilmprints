47988079a37168f48f2f198df87bc763ce4ce467
iamkennethu <iam.kennethu@gmail.com>
2022-12-31 22:53:47 +0800

    Commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import adv from "./videos/adv.mp4';
+import adv from "./videos/adv.mp4";
 
-const Main = () => {
+function Main() {
   return (
-    <div className='Main'>
-      <video src={adv} autoPlay loop muted></video>
-    </div>
-  )
+    <video src={adv} autoPlay loop muted></video>
+  );
 }
 
 export default Main;
