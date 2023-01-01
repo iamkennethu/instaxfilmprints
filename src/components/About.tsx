bf220cddd5307d2942475929a3d3072dc16a0def
iamkennethu <iam.kennethu@gmail.com>
2023-01-02 05:07:19 +0800

    Commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,13 +3,10 @@ import React from 'react';
 
 function About() {
   return (
-    <><div className='bg-video'>
+    <div className='bg-video'>
       <video loop autoPlay muted id='bg-video'>
         <source src={require('../videos/videoBg1.mp4')} type='video/mp4' /></video>
     </div>
-    <div>
-      <h1> Hello World</h1>
-      </div></>
   )
 }
 
