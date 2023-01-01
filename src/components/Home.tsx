1c59e28c223ec874e171a6472049eda3b3423151
iamkennethu <iam.kennethu@gmail.com>
2023-01-02 05:22:59 +0800

    Commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-
 function Home() {
   return (
     <div className='bg-video'>
       <video loop autoPlay muted id='bg-video'>
         <source src={require('../videos/videoBg.mp4')} type='video/mp4'/></video>
     </div>
+
   )
 }
 
