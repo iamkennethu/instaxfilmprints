e765d2c45da3acf3fe2d86c94afacb24aded7f6d
iamkennethu <iam.kennethu@gmail.com>
2023-01-02 06:28:18 +0800

    Commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-
 function Home() {
   return (
     <div className='bg-video'>
       <video loop autoPlay muted id='bg-video'>
       <source src={require('../videos/videoBg.mp4')} type='video/mp4'/></video>
-        <div className="background-image"
-          style={{ backgroundImage:"url(https://www.linkpicture.com/q/Website-Home-V.jpg)"}}>
+        <div className="background-image">
         </div>
     </div>
 
