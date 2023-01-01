bc5ca6bc596b8fcb7e536047c5050e1bcb4903dd
iamkennethu <iam.kennethu@gmail.com>
2023-01-02 06:11:44 +0800

    Commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+
 function Home() {
   return (
     <div className='bg-video'>
       <video loop autoPlay muted id='bg-video'>
-        <source src={require('../videos/videoBg.mp4')} type='video/mp4'/></video>
+      <source src={require('../videos/videoBg.mp4')} type='video/mp4'/></video>
+        <div className="background-image"
+          style={{ backgroundImage:"url(https://www.linkpicture.com/q/Website-Home-V.jpg)"}}>
+        </div>
     </div>
+
+
 
   )
 }
