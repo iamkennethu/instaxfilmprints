676840829057866cbe2131dc755660f1f457bfb7
iamkennethu <iam.kennethu@gmail.com>
2023-01-05 23:57:48 +0800

    Commit

--- src/footer/NavFooter.tsx
+++ src/footer/NavFooter.tsx
@@ -9,13 +9,14 @@ function Footer() {
     <div id='Footer'>
     <div className='Footer'>
         <h1> INSTAX FILM PRINTS PH.</h1>
-        <h4> Instax Film Prints PH. is a full-service of printing, event planning and design films specializing in artfully inspired event.</h4>
+        <p> Instax Film Prints PH. is a full-service of printing, event planning and design films <br/> specializing in artfully inspired event.</p>
         <h3> Sta. Rita, Guiguinto, Bulacan.</h3>
         <div className='header-btns'>
         <a href='/Inquire' className='header-btn'> INQUIRE</a>
         </div>
-        <IconContext.Provider value={{color: "white", size: "40px", }}>
+        <IconContext.Provider value={{ className: "icon", size:'40px'}}>
         <a href="https://www.instagram.com/instaxfilmprints"><CiInstagram/></a><a href="https://web.facebook.com/InstaxFilmPrints.PH"><RiFacebookBoxFill/></a>
+        <h4> follow along </h4>
         <h5> © 2022 INSTAX FILM PRINTS PH | ALL RIGHT RESERVED</h5>
         </IconContext.Provider>
     </div>
