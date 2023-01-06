d3b8c7b89b8c950036e11cbafcfbf831b8723807
iamkennethu <iam.kennethu@gmail.com>
2023-01-07 05:09:41 +0800

    Commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 
 function Home() {
   return (
+    <div className='main'>
     <div className='bg-video'>
       <video loop autoPlay muted id='bg-video'>
       <source src={require('../videos/videoBg.mp4')} type='video/mp4'/></video>
         <div className="fpage">
+        </div>
         </div>
     </div>
 
