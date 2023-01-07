143ae300f9f4e29c43234ef345b8ac8eb2e90718
iamkennethu <iam.kennethu@gmail.com>
2023-01-08 04:42:18 +0800

    Commit

---
+++ src/homeLandingPage/Home.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+// import webHome2 from '../images/webHome2.jpg';
+import Sample1 from '../images/picsamp.png';
+import Page2 from './Home2'
+
+
+function Home() {
+  return (
+    <><div className='main'>
+      <div className='bg-video'>
+        <video loop autoPlay muted id='bg-video'>
+          <source src={require('../videos/videoBg.mp4')} type='video/mp4' /></video>
+        <div className='fPage'>
+          <h5> WELCOME TO INSTAX FILM PRINTS PH. </h5>
+          <h1> PRINT IN FILMS <br /> THEY NEVER FADE. </h1>
+          <p> Keep your most cherished photos in polaroid films <br /> that nostalgia feels. </p>
+          {/* <img className='fpage' src={webHome2} alt=""/>  */}
+          <a href="/Guidelines"> <img className='fPage1Photos' src={Sample1} alt='' /> </a>
+          <a href="/Guidelines"> <img src={Sample1} alt='' /></a>
+          <a href="/Guidelines"> <img src={Sample1} alt='' /></a>
+        </div>
+      </div>
+    </div>
+    <Page2 />
+    </>
+     
+
+
+
+  )
+}
+
+export default Home;
