03d34f5b3e314927b498b930d458edf1c0153715
iamkennethu <iam.kennethu@gmail.com>
2023-01-08 03:31:13 +0800

    Commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import fpage from '../images/Website Home II.jpg';
+// import webHome2 from '../images/webHome2.jpg';
 import Sample1 from '../images/picsamp.png';
 
 
@@ -9,14 +9,18 @@ function Home() {
     <div className='bg-video'>
       <video loop autoPlay muted id='bg-video'>
       <source src={require('../videos/videoBg.mp4')} type='video/mp4'/></video>
-        <div className='fpage'>
-          {/* <img src={fpage} alt="WebpageI" /> */} 
-        <a href="/Guidelines"> <img src={Sample1} alt=''/> </a>
+        <div className='fPage'>
+          <h5> WELCOME TO INSTAX FILM PRINTS PH. </h5>
+          <h1> PRINT IN FILMS <br/> THEY NEVER FADE. </h1>
+          <p> Keep your most cherished photos in polaroid films <br/> that nostalgia feels. </p>
+          {/* <img className='fpage' src={webHome2} alt=""/>  */}
+        <a href="/Guidelines"> <img className='fPagePhoto' src={Sample1} alt=''/> </a>
         <a href="/Guidelines"> <img src={Sample1} alt=''/></a>
         <a href="/Guidelines"> <img src={Sample1} alt=''/></a>
         </div>
         </div>
-    </div>
+     </div>
+     
 
 
 
