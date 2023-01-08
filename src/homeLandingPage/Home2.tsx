296f55c5b205893fdfbff30bde5545794424b06e
iamkennethu <iam.kennethu@gmail.com>
2023-01-08 23:24:58 +0800

    Commit

--- src/homeLandingPage/Home2.tsx
+++ src/homeLandingPage/Home2.tsx
@@ -3,8 +3,7 @@ import React from 'react'
 function Home2() {
   return (
     <div className='fPage2'>
-      <div>
-        {/* <h1> Meet Ashley Gwen Martin </h1>
+        <h1> Meet Ashley Gwen Martin </h1>
         <h3> CREATIVE DIRECTOR & OWNER </h3>
         <hr />
         <p> With over a years of experience in the instax printing 
@@ -16,8 +15,7 @@ function Home2() {
             <br/> discerning clientele.</p>
         <p>Ashley Gwen’s touch make every prints incompatable 
             <br/> experience, transcending the expected to reflect the 
-            <br/> unique beauty of her client’s memories.</p> */}
-      </div>
+            <br/> unique beauty of her client’s memories.</p>
     </div>
   )
 }
