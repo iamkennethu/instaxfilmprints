a0385a4afb953565c8317c538494c4df6a601308
iamkennethu <iam.kennethu@gmail.com>
2023-01-08 22:27:32 +0800

    Commit

--- src/homeLandingPage/Home2.tsx
+++ src/homeLandingPage/Home2.tsx
@@ -4,7 +4,7 @@ function Home2() {
   return (
     <div className='fPage2'>
       <div>
-        <h1> Meet Ashley Gwen Martin </h1>
+        {/* <h1> Meet Ashley Gwen Martin </h1>
         <h3> CREATIVE DIRECTOR & OWNER </h3>
         <hr />
         <p> With over a years of experience in the instax printing 
@@ -16,9 +16,9 @@ function Home2() {
             <br/> discerning clientele.</p>
         <p>Ashley Gwen’s touch make every prints incompatable 
             <br/> experience, transcending the expected to reflect the 
-            <br/> unique beauty of her client’s memories.</p>
-            </div>
-            </div>
+            <br/> unique beauty of her client’s memories.</p> */}
+      </div>
+    </div>
   )
 }
 
