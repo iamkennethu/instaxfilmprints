d24cdabcdd49fc6a57b341719674fa3bdbefe880
iamkennethu <iam.kennethu@gmail.com>
2023-01-10 00:07:20 +0800

    Commit

--- src/homeLandingPage/Home5.tsx
+++ src/homeLandingPage/Home5.tsx
@@ -11,10 +11,14 @@ function Home5() {
     <div className='main'>
         <div className='fPage5'>
           <h1> WE CAN'T WAIT TO PRINT WITH YOU </h1>
+          <ul>
           <li><a href="/Inquire" > INQUIRE WITH US <hr/></a></li>
+          </ul>
           <a> <img src={picSamp1} alt='image1' className='pImage'/> </a>
           <a> <img src={picSamp2} alt='image2' className='pImage'/></a>
           <a> <img src={picSamp3} alt='image3' className='pImage'/></a>
+          <h4> Follow for more updates </h4>
+          <ol><a href="/Inquire" > @INSTAXFILMPRINTS.PH </a></ol>
       </div>
     </div>
 
