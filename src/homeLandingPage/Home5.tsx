71b8f0d06c99908c2be981cb4f01effe849ff189
iamkennethu <iam.kennethu@gmail.com>
2023-01-10 01:45:09 +0800

    Commit

--- src/homeLandingPage/Home5.tsx
+++ src/homeLandingPage/Home5.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import picSamp1 from '../images/picSamp1.png';
-import picSamp2 from '../images/picSamp2.png';
-import picSamp3 from '../images/picSamp3.png';
+import p5Sample1 from '../images/p5Sample1.png';
+import p5Sample2 from '../images/p5Sample2.png';
+import p5Sample3 from '../images/p5Sample3.png';
 
 
 
@@ -14,9 +14,9 @@ function Home5() {
           <ul>
           <li><a href="/Inquire" > INQUIRE WITH US <hr/></a></li>
           </ul>
-          <a> <img src={picSamp1} alt='image1' className='pImage'/> </a>
-          <a> <img src={picSamp2} alt='image2' className='pImage'/></a>
-          <a> <img src={picSamp3} alt='image3' className='pImage'/></a>
+          <a> <img src={p5Sample1} alt='image1' className='pImage'/> </a>
+          <a> <img src={p5Sample2} alt='image2' className='pImage'/></a>
+          <a> <img src={p5Sample3} alt='image3' className='pImage'/></a>
           <h4> Follow for more updates </h4>
           <ol><a href="/Inquire" > @INSTAXFILMPRINTS.PH </a></ol>
       </div>
