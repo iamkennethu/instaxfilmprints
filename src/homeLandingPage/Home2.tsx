29524e935a343511ccb2d3baa69a25c21a16940f
iamkennethu <iam.kennethu@gmail.com>
2023-01-09 18:59:37 +0800

    Commit

--- src/homeLandingPage/Home2.tsx
+++ src/homeLandingPage/Home2.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
+// import React from 'react'
 
-function Home2() {
-  return (
-    <div className='fPage2'>
-        <h1> Meet Ashley Gwen Martin </h1>
-        <h3> CREATIVE DIRECTOR & OWNER </h3>
-        <hr />
-        <p> With over a years of experience in the instax printing 
-            <br/> industry, Ashley Gwen Martin has become 
-            <br/> world-renowned for her impeccable taste and 
-            <br/> trend-setting creative direction. Her remarkable guest 
-            <br/> experience and conceptual printing works have 
-            <br/> transformed countless event spaces for an incredibly 
-            <br/> discerning clientele.</p>
-        <p>Ashley Gwen’s touch make every prints incompatable 
-            <br/> experience, transcending the expected to reflect the 
-            <br/> unique beauty of her client’s memories.</p>
-    </div>
-  )
-}
+// function Home2() {
+//   return (
+//     <div className='fPage2'>
+//         <h1> Meet Ashley Gwen Martin </h1>
+//         <h3> CREATIVE DIRECTOR & OWNER </h3>
+//         <hr />
+//         <p> With over a years of experience in the instax printing 
+//             <br/> industry, Ashley Gwen Martin has become 
+//             <br/> world-renowned for her impeccable taste and 
+//             <br/> trend-setting creative direction. Her remarkable guest 
+//             <br/> experience and conceptual printing works have 
+//             <br/> transformed countless event spaces for an incredibly 
+//             <br/> discerning clientele.</p>
+//         <p>Ashley Gwen’s touch make every prints incompatable 
+//             <br/> experience, transcending the expected to reflect the 
+//             <br/> unique beauty of her client’s memories.</p>
+//     </div>
+//   )
+// }
 
-export default Home2
+// export default Home2
