08b401f1728b7cdda68025cb15bfb06e95e6f5dd
iamkennethu <iam.kennethu@gmail.com>
2023-01-12 18:01:12 +0800

    Commit

--- src/aboutLandingPage/About.tsx
+++ src/aboutLandingPage/About.tsx
@@ -6,6 +6,13 @@ function About() {
     <div className='bg-video'>
       <video loop autoPlay muted id='bg-video'>
         <source src={require('../videos/videoBg1.mp4')} type='video/mp4' /></video>
+        <div className='aPage'>
+        <hr/>
+          <h5> ABOUT </h5>
+          <h1> We employ distinctive design and
+            <br/> Indelible to yield unparalleled artistry
+            <br/> and remarkable denouement </h1>
+        </div>
     </div>
   )
 }
