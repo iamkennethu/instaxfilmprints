fa42e0bf43d2ad4a856823028caa4ececbfc4d4a
iamkennethu <iam.kennethu@gmail.com>
2023-01-19 05:32:01 +0800

    Commit

--- src/specialofferLandingPage/SpecialOffer2.tsx
+++ src/specialofferLandingPage/SpecialOffer2.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import picSamp1 from '../images/picSamp1.png';
+import picSamp2 from '../images/picSamp2.png';
+import picSamp3 from '../images/picSamp3.png';
 
 function SpecialOffer2() {
   return (
     <div className='sPage2'>
         <h1> Offered Films <hr/></h1>
+        <a href="/Guidelines"> <img src={picSamp1} alt='' className='pImage'/> </a>
+          <a href="/Guidelines"> <img src={picSamp2} alt='' className='pImage'/></a>
+          <a href="/Guidelines"> <img src={picSamp3} alt='' className='pImage'/></a>
+          <br/>
+          <a href="/Guidelines"> <img src={picSamp1} alt='' className='pImage'/> </a>
+          <a href="/Guidelines"> <img src={picSamp2} alt='' className='pImage'/></a>
+          <a href="/Guidelines"> <img src={picSamp3} alt='' className='pImage'/></a>
     </div>
   )
 }
