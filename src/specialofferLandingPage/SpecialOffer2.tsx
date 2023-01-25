f26e38793b7faba1001aa7b264f24dab7a281d77
iamkennethu <iam.kennethu@gmail.com>
2023-01-26 01:47:45 +0800

    Commit

--- src/specialofferLandingPage/SpecialOffer2.tsx
+++ src/specialofferLandingPage/SpecialOffer2.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import picSamp1 from '../images/picSamp1.png';
-import picSamp2 from '../images/picSamp2.png';
-import picSamp3 from '../images/picSamp3.png';
+import P1 from '../images/P1.png';
+import P2 from '../images/P2.png';
+import P3 from '../images/P3.png';
+import P4 from '../images/P4.png';
+import P5 from '../images/P5.png';
+import P6 from '../images/P6.png';
 
 function SpecialOffer2() {
   return (
@@ -9,13 +12,13 @@ function SpecialOffer2() {
         <h1> Offered Films</h1>
         <hr/>
         <br/>
-          <a href="/SpecialOffer/Offer1"> <img src={picSamp1} alt='' className='sOffer1'/> </a>
-          <a href="/SpecialOffer/Offer2"> <img src={picSamp2} alt='' className='sOffer2'/></a>
-          <a href="/SpecialOffer/Offer3"> <img src={picSamp3} alt='' className='sOffer3'/></a>
+          <a href="/SpecialOffer/Offer1"> <img id='image1Promo' src={P1} alt=''/> </a>
+          <a href="/SpecialOffer/Offer2"> <img id='image2Promo'src={P2} alt=''/></a>
+          <a href="/SpecialOffer/Offer3"> <img id='image3Promo' src={P3} alt=''/></a>
           <br/>
-          <a href="/SpecialOffer/Offer4"> <img src={picSamp1} alt='' className='sOffer4'/> </a>
-          <a href="/SpecialOffer/Offer5"> <img src={picSamp2} alt='' className='sOffer5'/></a>
-          <a href="/SpecialOffer/Offer6"> <img src={picSamp3} alt='' className='sOffer6'/></a>
+          <a href="/SpecialOffer/Offer4"> <img id='image4Promo' src={P4} alt=''/> </a>
+          <a href="/SpecialOffer/Offer5"> <img id='image5Promo' src={P5} alt=''/></a>
+          <a href="/SpecialOffer/Offer6"> <img id='image6Promo' src={P6} alt=''/></a>
     </div>
   )
 }
